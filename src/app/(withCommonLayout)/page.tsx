0dc2aa4f6f3701d08fdbc1bb9dbf20e52b04ab4c
Jaman-Shah <jamanshah5400@gmail.com>
2025-03-06 22:42:46 +0600

    about section desigh finished

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,11 +1,13 @@
+import AboutSection from "@/components/HomePageComponents/AboutSection";
 import HeroSection from "@/components/HomePageComponents/HeroSection";
-import TourItemsSection from "@/components/HomePageComponents/TourItemsSection";
 
 export default function Home() {
   return (
     <div className="font-bold">
       <HeroSection />
-      <TourItemsSection />
+      <div className="px-2 md:px-16">
+        <AboutSection />
+      </div>
     </div>
   );
 }
