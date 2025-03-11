655b36043547b8b57a4d4a44306486c4ea8dafa7
Jaman-Shah <jamanshah5400@gmail.com>
2025-03-12 02:14:53 +0600

    some bugs fixes and geCurrentUser hook implemented for reusability code

---
+++ src/app/hooks/useGetCurrentUser.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { User } from "@/types/userType";
+import { getCurrentUserFromLocalStorage } from "@/utils/actions/getCurrentUserFromLocalStorage";
+import { getUserByEmail } from "@/utils/actions/getUserByEmail";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+const useGetCurrentUser = () => {
+  // Explicitly define the type of currentUser
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const userInToken = getCurrentUserFromLocalStorage();
+        const userEmail = userInToken?.email;
+
+        // Check if userEmail is defined
+        if (userEmail) {
+          const fetchedUser = await getUserByEmail(userEmail);
+          setCurrentUser(fetchedUser);
+        } else {
+          console.error("User email is undefined");
+          toast.error("User email not found in token");
+        }
+      } catch (err) {
+        console.error("Failed to fetch user data:", err);
+        toast.error("Failed to fetch user data");
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  useEffect(() => {
+    if (currentUser) {
+      console.log("user form dashboard", currentUser);
+    }
+  }, [currentUser]);
+
+  return { currentUser };
+};
+
+export default useGetCurrentUser;
