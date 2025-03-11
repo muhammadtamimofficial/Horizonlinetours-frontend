4cac277977bbe52422165784aa3bdf99682a25b5
Jaman-Shah <jamanshah5400@gmail.com>
2025-03-11 13:16:13 +0600

    protected route of nextauth finished

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -16,43 +16,11 @@ export const authOptions: NextAuthOptions = {
     }),
     // ...add more providers here
   ],
+  pages: {
+    signIn: "/login",
+  },
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    // async signIn({
-    //   user,
-    //   account,
-    // }: {
-    //   user: AuthUser;
-    //   account: Account | null;
-    // }) {
-    //   // Ensure account is not null and provider is google or github
-    //   if (account?.provider === "google" || account?.provider === "github") {
-    //     try {
-    //       // Ensure user email is defined
-    //       if (!user.email) {
-    //         console.log("No email provided by user.");
-    //         return false;
-    //       }
-
-    //       // Get user by email and create new user if not found
-    //       const existingUser = await getUserByEmail(user.email);
-    //       console.log(existingUser);
-    //       if (!existingUser) {
-    //         const userCreateRes = await createUser({
-    //           username: user.name || "No Name", // Fallback if no name is available
-    //           email: user.email,
-    //           image: user.image || "", // Fallback if no image is available
-    //         });
-    //         console.log(userCreateRes);
-    //       }
-    //       return true;
-    //     } catch (err) {
-    //       console.log("Error fetching or saving user", err);
-    //       return false;
-    //     }
-    //   }
-    //   return false;
-    // },
     async signIn({ account, profile }) {
       if (account?.provider === "google") {
         const newUser: {
